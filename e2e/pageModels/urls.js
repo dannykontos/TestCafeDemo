e294cedf14c868c1d6ff8379e6fc3341b27a8f95
DanaDC <67745151+DanaDC@users.noreply.github.com>
2021-01-26 11:15:02 +0200

    Update urls.js

--- e2e/pageModels/urls.js
+++ e2e/pageModels/urls.js
@@ -3,8 +3,8 @@ import { ClientFunction } from 'testcafe';
 class Url {
   constructor() {
     this.getCurrentPageUrl = ClientFunction(() => window.location.href);
-    this.baseUrl = 'https://use-beez.com/'
-    this.loginPageUrl = 'https://use-beez.com/account/login';
+    this.baseUrl = 'https://test.use-beez.com/'
+    this.loginPageUrl = 'https://test.use-beez.com/account/login';
     this.googleUrl = 'https://www.google.com';
   }
 }
